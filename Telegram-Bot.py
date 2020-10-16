3d793aaf70ea6ae5259bc1465866eb3614464855
SaiRakesh2001 <71959733+SaiRakesh2001@users.noreply.github.com>
2020-10-16 09:26:46 +0530

    Update Telegram-Bot.py

--- Telegram-Bot.py
+++ Telegram-Bot.py
@@ -1,30 +1,48 @@
-!pip install adafruit-io
+#!pip install adafruit-io
+#!pip install python-telegram-bot
+
 import os
-x = os.getenv('x')
-y = os.getenv('y')  #Use your own Adafruit Active Key
+
+from telegram.ext import Updater,CommandHandler
+import requests
+
+# 1. Python Code for Adafruit
+
+x = os.getenv('adafruit_io_username') #ADAFRUIT_IO_USERNAME
+y = os.getenv('adafruit_io_key') #ADAFRUIT_IO_KEY
+
 from Adafruit_IO import Client, Feed
 aio = Client(x,y)
-new = Feed(name='bot123')     #Creating a new feed
-result = aio.create_feed(new)
+feed = Feed(name = 'bot' ) #Create a feed
+result = aio.create_feed(feed)
 result
-from Adafruit_IO import Data
-!pip install python-telegram-bot
-from Adafruit_IO import Client,Data
-from telegram.ext import Updater,CommandHandler
-def on(bot,update):
-  chat_id = update.message.chat_id    
-  aio.create_data('bot123',Data(value = 1))
-  bot.send_message(chat_id =chat_id,text ="Lights On")
 
-def off(bot,update):
-  chat_id = update.message.chat_id
-  aio.create_data('bot123',Data(value = 0))
-  bot.send_message(chat_id =chat_id,text ="Lights Off")
+def send_value(indicator):
+  #Sending a value to the feed
+  from Adafruit_IO import Data
+  val = Data(value= indicator)
+  val_send = aio.create_data('bot', val)
+  
+# 2. Bot Coding with Python
 
-updater = Updater('1314501154:AAFupk7zdBSYmGlY2cDaSUdYLO4RAfF3TXs')     #Use Telegram Token HTTP API
-dp =updater.dispatcher
-dp.add_handler(CommandHandler('on',on))
-dp.add_handler(CommandHandler('off',off))
-updater.start_polling()
-updater.idle()
- 
+def light_on(bot,update):
+  url = 'https://p.kindpng.com/picc/s/61-617848_incandescent-light-bulb-lighting-transparency-and-translucency-yellow.png'
+  chat_id=update.message.chat_id
+  bot.send_message(chat_id,text = 'Turning on the Light')
+  bot.send_photo(chat_id,photo=url)
+  send_value(1)
+
+def light_off(bot,update):
+  url = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSHaNDJvg4tlgQPBlhCqRMzpesxagwVtOr-bA&usqp=CAU'
+  chat_id=update.message.chat_id
+  bot.send_message(chat_id,text = 'Turning off the Light')
+  bot.send_photo(chat_id,photo=url)
+  send_value(0)
+
+telegram_bot_api = os.getenv('telegram_bot_api')
+u = Updater(telegram_bot_api)
+dp = u.dispatcher
+dp.add_handler(CommandHandler('on',light_on))
+dp.add_handler(CommandHandler('off',light_off))
+u.start_polling()
+u.idle()
